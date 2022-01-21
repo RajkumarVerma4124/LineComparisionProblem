0ce7905ed37fbda6b21ed30235fbd9b461d31aec
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-21 22:18:25 +0530

    [Rajkumar] Added:- The code to calc lenth of line

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -8,9 +8,28 @@ namespace LineComparisionProblem
 {
     class Program
     {
+        //Declaring Variable
+        private static int x1, x2, y1, y2;
+        private static double length;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Line Comparison Computation Program.");
+            
+            //Getting the length of the line(UC1)
+            Console.Write("Enter the values of x1 : ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the values of y1 : ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter the values of x2 : ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+ 
+            Console.Write("Enter the values of y2 : ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+            
+            length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of the line is : " + Math.Round(length,2));
             Console.ReadLine();
         }
     }
